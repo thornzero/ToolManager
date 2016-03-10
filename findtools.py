ffee697571e44b3e44d12dfbfd8db131df160e2c
Daniel Thornburg <thornzero@gmail.com>
2016-03-10 10:18:33 -0500

    added .gitignore
    removed .idea from project

--- findtools.py
+++ findtools.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python
 
-import sys, getopt, re
+import sys
+import getopt
+import re
+
 
 def main(argv):
-  inputfile = ''
-  outputfile = '!setup'
-  p = re.compile(r'(T\d+)')
+    inputfile = ''
+    outputfile = '!setup.nc'
+    p = re.compile(r'(T\d+)')
 
-  try:
-    opts, args = getopt.getopt(argv,"hi:",["ifile="])
-  except getopt.GetoptError:
-    print 'test.py -i <inputfile>'
-    sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["ifile="])
+    except getopt.GetoptError:
+        print 'test.py -i <{0}>'.format(inputfile)
+        sys.exit(2)
 
-  for opt, arg in opts:
-    if opt == '-h':
-      print 'test.py -i <inputfile>'
-      sys.exit()
-    elif opt in ("-i", "--ifile"):
-      inputfile = arg
-      file = open(inputfile)
+    for opt, arg in opts:
+        if opt == '-h':
+            print "test.py -i <{0}>".format(inputfile)
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+            myfile = open(inputfile)
 
-      for line in file:
-        if p.findall(line) != '':
-        print(line)
+        for line in myfile:
+            if p.findall(line) != '':
+                print(line)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
